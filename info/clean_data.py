874b68e58271d737cd765ef64e6fb0aff8c85bfe
sealneaward <sealneaward@github.com>
2016-03-18 18:56:38 -0400

    Added columns to conferences. Onto creating tables for each conference.

--- info/clean_data.py
+++ info/clean_data.py
@@ -1,13 +1,15 @@
 import pandas
 
 acm = pandas.read_csv('acm.csv')
-acm.columns = ['Acronym', 'Conference']
-acm['Conference Host'] = 'ACM'
+acm.columns = ['Acronym', 'Name']
+acm['Organization'] = 'ACM'
 ieee = pandas.read_csv('ieee.csv')
-ieee.columns = ['Acronym', 'Conference']
-ieee['Conference Host'] = 'IEEE'
+ieee.columns = ['Acronym', 'Name']
+ieee['Organization'] = 'IEEE'
 conferences = ieee.append(acm, ignore_index=True)
-conferences.to_json('conferences.json',orient='records')
-
+conferences['ID'] = conferences.index
+conferences['Rating'] = 0
+conferences.to_json('conferences.json', orient='records')
+conferences.to_csv('conferences.csv', index=False)
 users = pandas.read_csv('users.csv')
 users.to_json('users.json',orient='records')
